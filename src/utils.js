d80b7bb3477cba188ef542e1bbe55d8291cc2af3
Col-By <collinbdev@outlook.com>
2025-06-19 18:57:34 -0500

    Navbar finished
    - Responsive design implemented
    - Navbar button functionality implemented
    - Utils getImageUrl fixed

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,5 @@
 // Takes a relative path and returns an absolute path URL.
 export const getImageUrl = (path) => {
-    return new URL(`assets/${path}`, import.meta.url).href;
-}
+    //return new URL(`../assets/${path}`, import.meta.url).href;
+    return `/assets/${path}`;
+};
