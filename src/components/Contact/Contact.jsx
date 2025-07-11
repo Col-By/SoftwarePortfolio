b49e642b0b3611701f4b5f0b3b5a7b77a5b69eb8
Col-By <collinbdev@outlook.com>
2025-07-11 15:12:18 -0500

    Contact Section

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+    return (
+        <footer className={styles.container} id="contact">
+            <div className={styles.text}>
+                <h2>Contact</h2>
+                <p>Feel free to reach out!</p>
+            </div>
+            <ul className={styles.links}>
+                <li className={styles.link}>
+                    <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon"/>
+                    <a href="mailto:collinbdev@outlook.com">collinbdev@outlook.com</a>
+                </li>
+                <li className={styles.link}>
+                    <img src={getImageUrl("contact/linkedinIcon.png")} alt="LinkedIn icon"/>
+                    <a href="https://www.linkedin.com/in/collin-boodry/">linkedin.com/collin-boodry</a>
+                </li>
+                <li className={styles.link}>
+                    <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon"/>
+                    <a href="https://github.com/Col-By">github.com/Col-By</a>
+                </li>
+            </ul>
+        </footer>
+    )
+    
+}
